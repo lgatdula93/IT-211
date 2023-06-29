4d57dc87aec7ac047c91ab1b7a828ac212ef9b29
Lourdes Gatdula <lgatdula93@gmail.com>
2023-06-28 21:19:40 -0700

    update with inline script

--- week1/script.js
+++ week1/script.js
@@ -1,19 +1,5 @@
-//alert("test");
-
-document.body.style.backgroundColor = "lavender";
-
 
     let firstName = "Lourdes ";
     let lastName = "Gatdula";
     let yearsOfStudy = "4 months";
     let goal = "become an expert in at least one programming language and become a Software Developer in a tech company";
-
-    let myName = "Hello! My name is " + firstName + lastName + ".";
-
-    let myStudies = "Over the past " + yearsOfStudy + ", I've been taking programming and business communication courses at Seattle Central College through a workforce training program called YearUp. " + "I've had experience with HTML/CSS prior to the program and am eager to work with Python and JavaScript over the next few weeks.";
-
-    let myGoal = "My goal after the program is to " + goal + ". " + "Aside from professional goals, some of my short-term goals include traveling internationally, visiting my parents more often in the Philippines, and learning woodwork!";
-
-    document.getElementById("my-name").innerHTML = myName;
-    document.getElementById("my-studies").innerHTML = myStudies;
-    document.getElementById("my-goal").innerHTML = myGoal;
