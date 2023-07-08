e31977c69b192e88d2f9fda0ba244b98aa44db43
Lourdes Gatdula <lgatdula93@gmail.com>
2023-07-07 23:07:39 -0700

    Update script to fix else if for two consonants

--- week2/script.js
+++ week2/script.js
@@ -1,5 +1,5 @@
 
-let firstVowels = "aeiouAEIOU"
+const vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O","U"];
 
 userInput.oninput = function() {
         
@@ -7,24 +7,21 @@ userInput.oninput = function() {
     let afterPig = ''
 
         if (beforePig.length <= 1) {
-            // bring an error message if the string is less than one character
-            afterPig = "Please enter a word that has more than one character."
-
-        } else if (!firstVowels.includes(beforePig.charAt(0))) {
-            // if the first letter is a consonant, move the consonant to the end of the word and add 'ay'
-            afterPig = beforePig.slice(1) + beforePig.charAt(0) + 'ay'
-
-        } else if (!firstVowels.includes(beforePig.charAt(0)) && !firstVowels.includes(beforePig.charAt(1))) {
-            // if the first two letters are consonants, bring them to the end of the word and add 'ay'
-            afterPig = beforePig.slice(2) + beforePig.slice(0, 2) + 'ay'
-                
-        }else {
-            // if the first letter is already a vowel, add 'way' at the end of the word
-            afterPig = beforePig + 'way'
+            afterPig = "Please enter a word that has more than one character.";
         }
+
+        else if (!vowels.includes(beforePig.charAt(0)) && !vowels.includes(beforePig.charAt[1])) {
+            afterPig = beforePig.slice(2) + beforePig.slice(0, 2) + 'ay';
+        }
+
+        else if (!vowels.includes(beforePig.charAt(0))) {
+            afterPig = beforePig.slice(1) + beforePig.charAt(0) + 'ay';      
+        }
+
+        else {
+            afterPig = beforePig + 'way'
+        } 
 
            
         pigConverted.innerHTML = afterPig;
     };
-
-
